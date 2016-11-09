6941939b1b5baa36d849a8795ef4322936d04d86
maxevilmind <maxevilmind@gmail.com>
2016-11-10 02:57:14 +0600

    arkanoid remake

---
+++ Assets/Scripts/BrickController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickController : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision collision) {
+		GameObject gameObject = collision.gameObject;
+		if (gameObject.CompareTag ("Ball")) {
+			Destroy (this.gameObject);
+			GameController.score_counter++;
+		}
+	}
+}
